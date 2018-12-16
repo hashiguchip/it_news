5a17efe6a7061949a7a3823f15cf67f88ec2c8b3
hashiguchip <hashiguchip@gmail.com>
2018-12-16 17:44:03 +0900

    途中

---
+++ src/components/ListPage.vue
@@ -0,0 +1,31 @@
+<!-- src/components/Hello.vue -->
+
+<template>
+    <ul id="example-1">
+        <li v-for="item in list">
+            {{ item.url }}
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import Vuex from 'vuex'
+
+    export default Vue.extend({
+        props: ['name', 'initialEnthusiasm'],
+        data() {
+            return {
+                list: this.$store.state.list,
+            }
+        },
+    });
+</script>
+
+<style lang="scss">
+    .parents {
+        .greeting {
+            font-size: 120px;
+        }
+    }
+</style>
