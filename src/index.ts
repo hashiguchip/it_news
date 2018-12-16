beadcf9b4ceb09d8da0fe20a5da31afc25951e0a
hashiguchip <hashiguchip@gmail.com>
2018-12-16 15:39:30 +0900

    [add] hello vuex

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import Vue from "vue";
+import Vuex from 'vuex'
 import HelloComponent from "./components/Hello.vue";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment: state => state.count++,
+        decrement: state => state.count--
+    }
+})
+
 
 let v = new Vue({
     el: "#app",
+    store,//vuex
     template: `
     <div>
         Name: <input v-model="name" type="text">
         <hello-component :name="name" :initialEnthusiasm="5" />
     </div>
     `,
-    data: { name: "World" },
+    data: {name: "World"},
     components: {
         HelloComponent
     }
